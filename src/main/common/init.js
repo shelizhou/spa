4ae73c3f6aaa20462619f4f2f96ab79bec459d56
she <shelizhou88@gmail.com>
2016-07-01 10:06:43 +0800

    all

--- src/main/common/init.js
+++ src/main/common/init.js
@@ -17,22 +17,12 @@ define([],function ( ) {
 
 
         // 由于某些android手机还是对active 兼容有问题，固还是采用js兼容
-        $wrap.on("touchstart", ".m_active_bgw", function(){
-            $(this).addClass("m_active_bgw_active");
-        }).on("touchend touchcancel touchmove", ".m_active_bgw", function(){
-            $(this).removeClass("m_active_bgw_active");
-        });
-        $wrap.on("touchstart", ".m_active_bgg", function(){
-            $(this).addClass("m_active_bgg_active");
-        }).on("touchend touchcancel touchmove", ".m_active_bgg", function(){
-            $(this).removeClass("m_active_bgg_active");
+        $wrap.on("touchstart", ".J__active", function(){
+            $(this).addClass("m_active");
+        }).on("touchend touchcancel touchmove", ".J__active", function(){
+            $(this).removeClass("m_active");
         });
 
-        $wrap.on("touchstart", ".m_active_op", function(){
-            $(this).addClass("m_active_op_active");
-        }).on("touchend touchcancel touchmove", ".m_active_op", function(){
-            $(this).removeClass("m_active_op_active");
-        });
 
 
         // 页面控制
